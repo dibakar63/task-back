6bbb07768b6706078e05810ae1aa0aacc9ee0913
Dibakar Dey <110836157+dibakar63@users.noreply.github.com>
2023-09-20 09:36:00 +0530

    Update router.js

--- router.js
+++ router.js
@@ -1,11 +1,11 @@
 const authcontroller=require('./controller');
 const router=require('express').Router();
 
-router.post('/add',authcontroller.userController);
+router.post('/add',authcontroller.bookController);
 router.get('/get',authcontroller.getAllData);
 router.get('/get/:id',authcontroller.getById);
-router.put('/update/:id',authcontroller.updateUsers);
-router.delete('/delete/:id',authcontroller.deleteUsers);
+router.put('/update/:id',authcontroller.updateBooks);
+router.delete('/delete/:id',authcontroller.deleteBooks);
 
 
 module.exports=router
